5189f44692286422823f062298c611be68568ffc
shimy8617 <cristinashim@yahoo.com.ar>
2022-02-07 16:08:04 -0300

    me

--- src/index.js
+++ src/index.js
@@ -6,31 +6,29 @@ import './App.css';
 import MealList from "./MealList";
 
 
- const MySelect = ({ label, ...props }) => {
-   const [field, meta] = useField(props);
-   return (
-     <div>
-       <label htmlFor={props.id || props.name}>{label}</label>
-       <select {...field} {...props} />
-       {meta.touched && meta.error ? (
-         <div className="error">{meta.error}</div>
-       ) : null}
+const MySelect = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+ <div>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <select {...field} {...props} />
+      {meta.touched && meta.error ? (
+     <div className="error">{meta.error}</div>
+     ) : null}
      </div>
    );
  };
 
  
  // And now we can use these
- const SignupForm = () => {
-   
- const [mealData, setMealData] = useState(null); 
- const [diet, setDiet] = useState("");
- const apiUrl = "40302453062c462299983b93e3e85f00";
+ const MealForm = () => {
+
+const [mealData, setMealData] = useState(null); 
+const [diet, setDiet] = useState("");
+const apiUrl = "40302453062c462299983b93e3e85f00";
 
  function getMealData() {
-  fetch(
-    `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiUrl}&timeFrame=day&diet=${diet}`
-  )
+  fetch(`https://api.spoonacular.com/mealplanner/generate?apiKey=${apiUrl}&timeFrame=day&diet=${diet}`)
     .then((response) => response.json())
     .then((data) => {
       setMealData(data);
@@ -45,46 +43,46 @@ function handleChange(e) {
   console.log(e);
 }
 
-   return (
-     <>
-       <h1>Elige un Menu!</h1>
-       <Formik
-         initialValues={{
-           FoodType: '', // added for our select
-         }}
-         validationSchema={Yup.object({
+return (
+  <>
+    <h1>Elige un Menu!</h1>
+    <Formik
+      initialValues={{
+        FoodType: '', // added for our select
+      }}
+      validationSchema={Yup.object({
           foodType: Yup.string()
-             .oneOf(
-               ['vegan', 'gluten-free', 'vegetarian']
-             )
-             .required('Required'),
-         })}
-         onSubmit={(values, { setSubmitting }) => {
-           setTimeout(() => {
-             alert(JSON.stringify(values, null, 2));
-             setSubmitting(false);
-           }, 400);
-         }}
-       >
-         <Form> 
-           <MySelect onChange={handleChange} label="Food Type" name="foodType">
-             <option value="">Select a food type</option>
-             <option  value="vegan">Vegan</option>
-             <option  value="gluten-free">Gluten-Free</option>
-             <option  value="vegetarian">Vegetarian</option>
-           </MySelect>
- 
-           <button onClick={getMealData} type="submit">Buscar</button>
-         </Form>
-       </Formik>
-       {mealData && <MealList mealData={mealData} />}
-     </>
-   );
- };
- 
- function App() {
+            .oneOf(
+              ['vegan', 'gluten-free', 'vegetarian']
+            )
+.required('Required'),
+})}
+onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+        }, 400);
+        }}
+      >
+        <Form> 
+          <MySelect onChange={handleChange} label="Food Type" name="foodType">
+            <option value="">Select a food type</option>
+            <option  value="vegan">Vegan</option>
+            <option  value="gluten-free">Gluten-Free</option>
+            <option  value="vegetarian">Vegetarian</option>
+          </MySelect>
+
+          <button onClick={getMealData} type="submit">Buscar</button>
+        </Form>
+      </Formik>
+      {mealData && <MealList mealData={mealData} />}
+    </>
+  );
+};
+
+function App() {
   return (
-  <SignupForm />
+  <MealForm />
     );
 }
 
