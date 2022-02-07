fab42d8e3766fa67b10db31447e23fe309382241
shimy8617 <cristinashim@yahoo.com.ar>
2022-02-07 12:57:09 -0300

    me

--- src/App.js
+++ src/App.js
@@ -1,31 +1,72 @@
-import React from 'react';
-import { useFormik } from 'formik';
+import React, { useState } from "react";
+import './App.css';
+import axios from 'axios';
+import instance from './axios/config';
+import MealList from "./MealList";
 
-const App = () => {
-  // Pass the useFormik() hook initial form values and a submit function that will
-  // be called when the form is submitted
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-    },
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+  const [diet, setDiet] = useState("");
+  const apiUrl = "40302453062c462299983b93e3e85f00";
+
+  function getMealData() {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiUrl}&timeFrame=day&targetCalories=${calories}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMealData(data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }
+
+  function getMealImage() {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiUrl}&timeFrame=day&targetCalories=${calories}&diet=${diet}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMealData(data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('You clicked submit.');
+    let connection = axios.post(instance.baseURL)
+    return connection;
+  }
+
+  function handleChange(e) {
+    setCalories(e.target.value);
+    setDiet(e.target.value);
+  }
+
+  
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Type of Food</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-
-      <button type="submit">Submit</button>
-    </form>
+    <div className="App">
+      <section className="controls">
+        <input
+          type="number"
+          placeholder="Calories (e.g. 2000)"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Ex. Vegan"
+          onChange={handleChange}
+        />
+        <button onClick={getMealData}>Get Daily Meal Plan</button>
+      </section>
+      {mealData && <MealList mealData={mealData} />}
+    </div>
   );
-};
+}
 
 export default App;
