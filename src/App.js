448c6b71dc67ef83cb94ad7b7327683c3cf29ae0
shimy8617 <cristinashim@yahoo.com.ar>
2022-02-08 10:04:37 -0300

    me

--- src/App.js
+++ src/App.js
@@ -1,72 +1,76 @@
-import React, { useState } from "react";
+import ReactDOM from 'react-dom';
+import { Formik, Form, useField } from 'formik';
+import  {useState} from 'react';
+import * as Yup from 'yup';
 import './App.css';
-import axios from 'axios';
-import instance from './axios/config';
 import MealList from "./MealList";
 
-function App() {
-  const [mealData, setMealData] = useState(null);
-  const [calories, setCalories] = useState(2000);
-  const [diet, setDiet] = useState("");
-  const apiUrl = "40302453062c462299983b93e3e85f00";
 
-  function getMealData() {
-    fetch(
-      `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiUrl}&timeFrame=day&targetCalories=${calories}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setMealData(data);
-      })
-      .catch(() => {
-        console.log("error");
-      });
-  }
 
-  function getMealImage() {
-    fetch(
-      `https://api.spoonacular.com/mealplanner/generate?apiKey=${apiUrl}&timeFrame=day&targetCalories=${calories}&diet=${diet}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setMealData(data);
-      })
-      .catch(() => {
-        console.log("error");
-      });
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log('You clicked submit.');
-    let connection = axios.post(instance.baseURL)
-    return connection;
-  }
-
-  function handleChange(e) {
-    setCalories(e.target.value);
-    setDiet(e.target.value);
-  }
+ 
+ // And now we can use these
+ const MealForm = () => {
 
   
-  return (
-    <div className="App">
-      <section className="controls">
-        <input
-          type="number"
-          placeholder="Calories (e.g. 2000)"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          placeholder="Ex. Vegan"
-          onChange={handleChange}
-        />
-        <button onClick={getMealData}>Get Daily Meal Plan</button>
-      </section>
-      {mealData && <MealList mealData={mealData} />}
-    </div>
-  );
+const apiUrl = "40302453062c462299983b93e3e85f00";
+
+const [mealData, setMealData] = useState(null); 
+
+const [diet, setDiet] = useState(["Vegan", 'gluten-free', 'vegetarian']);
+const Add = diet.map(Add => Add)
+const handleChange= (e) => {console.clear(); console.log((diet[e.target.value]))}
+
+ function getMealData() {
+  fetch(`https://api.spoonacular.com/mealplanner/generate?apiKey=${apiUrl}&timeFrame=day&diet=${diet}`)
+    .then((response) => response.json())
+    .then((data) => {
+      setMealData(data);
+    })
+    .catch(() => {
+      console.log("error");
+    });
 }
 
-export default App;
+
+
+return (
+  <>
+    <h1>Elige un Menu!</h1>
+    <Formik
+      initialValues={{
+        FoodType: '', // added for our select
+      }}
+      validationSchema={Yup.object({
+          foodType: Yup.string()
+            .oneOf(
+              ['vegan', 'gluten-free', 'vegetarian']
+            )
+.required('Required'),
+})}
+onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+        }, 400);
+        }}
+      >
+        <Form> 
+          <select onChange={e => handleChange(e)} label="Food Type" name="foodType">
+          {
+        Add.map((address, key) => <option value={key}>{address}</option>)
+      }
+          </select>
+
+          <button onClick={getMealData} type="submit">Buscar</button>
+        </Form>
+      </Formik>
+      {mealData && <MealList mealData={mealData} />}
+    </>
+  );
+};
+
+export default function App() {
+  return (
+  <MealForm />
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,79 +1,11 @@
-import ReactDOM from 'react-dom';
-import { Formik, Form, useField } from 'formik';
-import  {useState} from 'react';
-import * as Yup from 'yup';
-import './App.css';
-import MealList from "./MealList";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-
-
- 
- // And now we can use these
- const MealForm = () => {
-
-  
-const apiUrl = "40302453062c462299983b93e3e85f00";
-
-const [mealData, setMealData] = useState(null); 
-
-const [diet, setDiet] = useState(["Vegan", 'gluten-free', 'vegetarian']);
-const Add = diet.map(Add => Add)
-const handleChange= (e) => {console.clear(); console.log((diet[e.target.value]))}
-
- function getMealData() {
-  fetch(`https://api.spoonacular.com/mealplanner/generate?apiKey=${apiUrl}&timeFrame=day&diet=${diet}`)
-    .then((response) => response.json())
-    .then((data) => {
-      setMealData(data);
-    })
-    .catch(() => {
-      console.log("error");
-    });
-}
-
-
-
-return (
-  <>
-    <h1>Elige un Menu!</h1>
-    <Formik
-      initialValues={{
-        FoodType: '', // added for our select
-      }}
-      validationSchema={Yup.object({
-          foodType: Yup.string()
-            .oneOf(
-              ['vegan', 'gluten-free', 'vegetarian']
-            )
-.required('Required'),
-})}
-onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-        }, 400);
-        }}
-      >
-        <Form> 
-          <select onChange={e => handleChange(e)} label="Food Type" name="foodType">
-          {
-        Add.map((address, key) => <option value={key}>{address}</option>)
-      }
-          </select>
-
-          <button onClick={getMealData} type="submit">Buscar</button>
-        </Form>
-      </Formik>
-      {mealData && <MealList mealData={mealData} />}
-    </>
-  );
-};
-
-function App() {
-  return (
-  <MealForm />
-    );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
