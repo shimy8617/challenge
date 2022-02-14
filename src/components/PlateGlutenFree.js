63628871d06464e3ffb8328ef7f34ac3195345e7
Cristina Shim <84110240+shimy8617@users.noreply.github.com>
2022-02-14 09:14:53 -0300

    Update PlateGlutenFree.js

--- src/components/PlateGlutenFree.js
+++ src/components/PlateGlutenFree.js
@@ -37,7 +37,7 @@ const PlateGlutenFree = (props) => {
   return (
     <div className="container">
       <div className="card-text">
-        {title}
+        <div className="titleMeal">{title}</div>
         <img src={image} alt="" />
         <ul className="listInfo">
           <li>Servings: {servings}</li>
